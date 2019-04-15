dde28b3710a2db85a8d59cf76793dfae96dea0d3
sadeerthaer <sadeerthaer@gmail.com>
2019-04-15 12:11:08 +0200

    Publish
    A program enters the coordinates of a point (x, y) and determines whether it is located on one of the axes, within a quadrant, or on the principle point.
    ￼￼￼

---
+++ CoordinaterHelper/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace CoordinaterHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            Title = "Determine (x,y) place"; Write("Enter x: ");
+
+            x = Convert.ToDouble(ReadLine()); Write("Enter y: ");
+            y = Convert.ToDouble(ReadLine()); if (x > 0 && y > 0)
+            {
+                WriteLine("The point (" + x + "," + y + ") is on the 1st quarter!");
+            }
+            else if (x < 0 && y > 0)
+                WriteLine("The point (" + x + "," + y + ") is on the 2nd quarter!");
+            else if (x < 0 && y < 0)
+                WriteLine("The point (" + x + "," + y + ") is on the 3rd quarter!");
+            else if (x > 0 && y < 0)
+                WriteLine("The point (" + x + "," + y + ") is on the 4th quarter!");
+        }
+    }
+}
